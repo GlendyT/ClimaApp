7d289274466b609df41824f351ca0b95945a5042
Glendy <glendytuyuc@gmail.com>
2023-09-21 21:06:24 -0600

    primer cambio

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,44 @@
+
+const Formulario = () => {
+  return (
+    <div className="contenedor">
+        <form>
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad</label>
+                <input
+                   type="text"
+                   id="ciudad"
+                   name="ciudad"
+                />
+            </div>
+
+            <div className="campo">
+                <label htmlFor="pais">Pais</label>
+                <select
+                   id="pais"
+                   name="pais"
+                >
+                    <option value="">Seleccione un Pais</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">Mexico</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Peru</option>
+                    <option value="KR">Corea del Sur</option>
+                    <option value="BE">Belgica</option>
+
+                </select>
+            </div>
+
+            <input
+              type="submit"
+              value="Consultar Clima"
+            />
+        </form>
+    </div>
+  )
+}
+
+export default Formulario
