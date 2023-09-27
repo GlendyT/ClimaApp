d5895fcabeed0cee8c4af48fddc1cbe41e1e9c3c
Glendy <glendytuyuc@gmail.com>
2023-09-27 13:53:41 -0600

    agregar el resultado

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from "react";
+import axios from "axios"
 
 const ClimaContext = createContext()
 
@@ -8,6 +9,8 @@ const ClimaProvider = ({children}) => {
         ciudad: "",
         pais:""
     })
+
+    const [resultado, setResultado] = useState({})
      
     const datosBusqueda = e => {
         setBusqueda({
@@ -16,11 +19,33 @@ const ClimaProvider = ({children}) => {
         })
     }
 
+    const consultarClima = async datos => {
+        try{
+            const {ciudad, pais} = datos
+            const appId = import.meta.env.VITE_API_KEY
+
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+
+            const {data } = await axios (url)
+            const {lat, lon} = data[0]
+
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            
+            const { data: clima } = await axios(urlClima)
+            setResultado(clima)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <ClimaContext.Provider
            value={{
             busqueda,
-            datosBusqueda
+            datosBusqueda,
+            consultarClima,
+            resultado
            }}
         >
             {children}
